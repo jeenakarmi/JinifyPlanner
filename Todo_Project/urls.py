61cc2d9f4302c852492b88e8cff1c3e3575fe736
jeenakarmi <jinanakarmi@gmail.com>
2024-02-16 18:52:10 +0545

    Add form,retrive data & solve url error

--- Todo_Project/urls.py
+++ Todo_Project/urls.py
@@ -14,10 +14,23 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# from django.contrib import admin
+# from django.urls import path,include
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('todos/',include('todos.urls'))
+# ]
+
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 
 urlpatterns = [
+    path('', include('todos.urls')),  # Include todos app URLs
     path('admin/', admin.site.urls),
-    path('todos/',include('todos.urls'))
 ]
+
+
+
+
+
--- todos/urls.py
+++ todos/urls.py
@@ -1,6 +1,19 @@
+# from django.urls import path
+# from . import views 
+# urlpatterns = [
+#     path('list/',views.list_todo_items),
+#     path('insert_todo/',views.insert_todo_item,name ='insert_todo_item')
+# ]
+
 from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('list/',views.list_todo_items)
+    path('', views.list_todo_items, name='list_todo_items'),  # Root URL pattern
+    path('list/', views.list_todo_items, name='list_todo_items'),
+    path('insert_todo/', views.insert_todo_item, name='insert_todo_item'),
 ]
+
+
+
+
