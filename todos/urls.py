6d752ce31ce9290249a8614979759cdb0c2ba3fd
jeenakarmi <jinanakarmi@gmail.com>
2024-06-29 08:11:06 +0545

    Update .gitignore to ignore __pycache__ directory

--- todos/urls.py
+++ todos/urls.py
@@ -10,7 +10,7 @@ from . import views
 
 urlpatterns = [
     path('', views.list_todo_items, name='list_todo_items'),  # Root URL pattern #entry point for accessing list of todo items within web application.
-    path('list/', views.list_todo_items, name='list_todo_items'), # to access a list of todo items
+    # path('list/', views.list_todo_items, name='list_todo_items'), # to access a list of todo items
     path('insert_todo/', views.insert_todo_item, name='insert_todo_item'),  #to handle inserting new todo items
 
     #pass value of  int todo_id from view
